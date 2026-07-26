c7e0a3fbf03aa39a5534c01b447a3412402d19ff
Harshit Pawar <hpchess64@gmail.com>
2026-07-26 13:58:03 +0530

    feat: add night condition icons and modularize ASCII icons

--- src/weather/ui/conditions.py
+++ src/weather/ui/conditions.py
@@ -1,6 +1,202 @@
 from dataclasses import dataclass
 
+STAR = "[yellow blink]✧[/]"
+BOLT = "[yellow blink]⚡[/]"
+SNOW = "[white]*[/]"
+HSNOW = "[bold white]*[/]"
+HAIL = "[white]•[/]"
+DROP = "[cornflower_blue]ʻ[/]"
+RDROP = "[cornflower_blue]‚ʻ[/]"
+HDROP = "[dodger_blue1]‚ʻ[/]"
 
+# fmt:off
+_UNKNOWN = (
+    "    .--.   \n"
+    "      __)  \n"
+    "    (      \n"
+    "     `-᾿   \n"
+    "      •      "
+)
+
+_SUN = (
+    "[yellow]     \\    /    [/]\n"
+    "[yellow]      .--.      [/]\n"
+    "[yellow]  __ (    ) __  [/]\n"
+    "[yellow]      `--`      [/]\n"
+    "[yellow]     /    \\    [/]"
+)
+
+_DAY_LIGHT_CLOUD = (
+    "[yellow]   \\__/                   [/]\n"
+    '[yellow] __/  )[/][grey70]--.     [/]\n'
+    "[yellow]   \\_[/][grey70](    ).  [/]\n"
+    "[yellow]   /[/][grey70](___(___)   [/]\n"
+)
+
+_DAY_HEAVY_CLOUD = (
+    "[yellow]   \\__/                   [/]\n"
+    '[yellow] __/  )[/][grey62]--.     [/]\n'
+    "[yellow]   \\_[/][grey62](    ).  [/]\n"
+    "[yellow]   /[/][grey62](___(___)   [/]\n"
+)
+
+_MOON = (
+    f"[cyan]  {STAR} .._  [/]\n"
+    "[cyan]  .' .-` [/]\n"
+    f"[cyan] /  /   {STAR} [/]\n"
+    "[cyan] \\  \\  [/]\n"
+    f"[cyan]{STAR} '._`_. [/]"
+)
+
+_NIGHT_LIGHT_CLOUD = (
+    "[cyan]  ,-,                      [/]\n"
+    "[cyan] /.( [/][grey70]  .--.     [/]\n"
+    "[cyan] \\ {[/][grey70]  (    ).  [/]\n"
+    "[cyan]  `-`[/][grey70](___(___)  [/]\n"
+)
+
+
+_NIGHT_HEAVY_CLOUD = (
+    "[cyan]  ,-,                      [/]\n"
+    "[cyan] /.( [/][grey62]  .--.     [/]\n"
+    "[cyan] \\ {[/][grey62]  (    ).  [/]\n"
+    "[cyan]  `-`[/][grey62](___(___)  [/]\n"
+)
+
+_LIGHT_CLOUD = (
+    "[grey70]    .--.     [/]\n"
+    "[grey70]   (    ).   [/]\n"
+    "[grey70]  (___(___)  [/]\n"
+)
+
+_HEAVY_CLOUD = (
+    "[grey62]    .--.     [/]\n"
+    "[grey62]   (    ).   [/]\n"
+    "[grey62]  (___(___)  [/]\n"
+)
+
+_OVERCAST = (
+    "                         \n"
+    "[grey62]    .--.      [/]\n"
+    "[grey62]  _(    )..   [/]\n"
+    "[grey62] (___.__)__)  [/]\n"
+)
+
+_FOG = (
+    "                         \n"
+    "[grey74] _ - _ - _ -  [/]\n"
+    "[grey74]  _ - _ - _   [/]\n"
+    "[grey74] _ - _ - _ -  [/]\n"
+)
+
+_RIME_FOG = (
+    "                                     \n"
+    f"[grey74] _ - _ {SNOW} _ -        [/]\n"
+    f"[grey74]  _ {SNOW} _ - _ {SNOW}  [/]\n"
+    f"[grey74] _ - _ {SNOW} _ -        [/]\n"
+)
+
+_DRIZZLE_LIGHT = (
+    f"   {DROP}  {DROP}  {DROP}\n"
+    f" {DROP}  {DROP}  {DROP}"
+)
+
+_DRIZZLE = (
+    f"   {DROP} {DROP} {DROP} {DROP}\n"
+    f" {DROP} {DROP} {DROP} {DROP}"
+)
+
+_DRIZZLE_HEAVY = (
+    f"  {HDROP}{HDROP}{HDROP}{HDROP}\n"
+    f"  {HDROP}{HDROP}{HDROP}{HDROP}"
+)
+
+_RAIN = (
+    f"  {RDROP}{RDROP}{RDROP}{RDROP}\n"
+    f"  {RDROP}{RDROP}{RDROP}{RDROP}"
+)
+
+_RAIN_HEAVY = (
+    f" {HDROP}{HDROP}{HDROP}{HDROP}{HDROP}\n"
+    f"{HDROP}{HDROP}{HDROP}{HDROP}{HDROP}"
+)
+
+_FREEZING_LIGHT = (
+    f"   {DROP} {SNOW} {DROP} {SNOW}\n"
+    f"  {SNOW} {DROP} {SNOW} {DROP}"
+)
+
+_FREEZING_HEAVY = (
+    f"  {RDROP} {SNOW} {RDROP} {SNOW}\n"
+    f" {SNOW} {RDROP} {SNOW} {RDROP}"
+)
+
+_FREEZING_RAIN_HEAVY = (
+    f"  {HDROP} {SNOW} {HDROP} {SNOW}\n"
+    f" {SNOW} {HDROP} {SNOW} {HDROP}"
+)
+
+_SNOW_LIGHT = (
+    f"   {SNOW}   {SNOW}\n"
+    f"  {SNOW}   {SNOW}"
+)
+
+_SNOW = (
+    f"   {SNOW}  {SNOW}  {SNOW}\n"
+    f"  {SNOW}  {SNOW}  {SNOW}"
+)
+
+_SNOW_HEAVY = (
+    f"   {HSNOW} {HSNOW} {HSNOW} {HSNOW}\n"
+    f"  {HSNOW} {HSNOW} {HSNOW} {HSNOW}"
+)
+
+_SNOW_GRAINS = (
+    f"   {HAIL}  {HAIL}  {HAIL}\n"
+    f"  {HAIL}  {HAIL}  {HAIL}"
+)
+
+_SHOWERS_LIGHT = (
+    f"     {DROP}  {DROP}  {DROP}\n"
+    f"    {DROP}  {DROP}  {DROP}"
+)
+
+_SHOWERS = (
+    f"     {DROP} {DROP} {DROP} {DROP}\n"
+    f"    {DROP} {DROP} {DROP} {DROP}"
+)
+
+_SHOWERS_HEAVY = (
+    f"    {HDROP}{HDROP}{HDROP}{HDROP}\n"
+    f"   {HDROP}{HDROP}{HDROP}{HDROP}"
+)
+
+_SNOW_SHOWERS_LIGHT = (
+    f"     {HSNOW}  {HSNOW}  {HSNOW}\n"
+    f"    {HSNOW}  {HSNOW}  {HSNOW}"
+)
+
+_SNOW_SHOWERS_HEAVY = (
+    f"    {HSNOW} {HSNOW} {HSNOW} {HSNOW}\n"
+    f"   {HSNOW} {HSNOW} {HSNOW} {HSNOW}"
+)
+
+_STORM = (
+    f"    {DROP} {DROP} {BOLT} {DROP}\n"
+    f"   {DROP} {BOLT} {DROP} {DROP}"
+)
+
+_STORM_HAIL = (
+    f"    {HAIL} {DROP} {BOLT} {HAIL}\n"
+    f"   {DROP} {BOLT} {HAIL} {DROP}"
+)
+
+_STORM_SEVERE = (
+    f"   {HDROP} {HAIL} {BOLT}{HDROP} {HAIL}\n"
+    f"  {HAIL} {BOLT} {HAIL} {BOLT}{HDROP}"
+)
+
+# fmt: on
 @dataclass(frozen=True)
 class WeatherCondition:
     label: str
@@ -8,312 +204,78 @@ class WeatherCondition:
 
 
 _WEATHER_CONDITIONS = {
-    -1: WeatherCondition(
-        label="Unknown",
-        icon=(
-            "    .--.        \n"
-            "      __)      \n"
-            "    (          \n"
-            "     `-᾿        \n"
-            "      •         "
-        ),
-    ),
-    0: WeatherCondition(
-        label="Clear sky",
-        icon=(
-            "[yellow]     \\    /      [/yellow]\n"
-            "[yellow]      .--.       [/yellow]\n"
-            "[yellow]  __ (    ) __    [/yellow]\n"
-            "[yellow]      `--`      [/yellow]\n"
-            "[yellow]     /    \\      [/yellow]"
-        ),
-    ),
-    1: WeatherCondition(
-        label="Mainly clear",
-        icon=(
-            "[yellow]    \\__/     [/yellow]    \n"
-            "[yellow]  __/  )[/yellow][grey70].--.     [/grey70]\n"
-            "[yellow]    \\_[/yellow][grey70](     ).   [/grey70]\n"
-            "[yellow]    /[/yellow][grey70](___(___) [/grey70]\n"
-            "                 "
-        ),
-    ),
-    2: WeatherCondition(
-        label="Partly cloudy",
-        icon=(
-            "[yellow]    \\__/     [/yellow]    \n"
-            "[yellow]  __/  [/yellow][grey70]).--.     [/grey70]\n"
-            "[yellow]    \\_[/yellow][grey70](     ).   [/grey70]\n"
-            "[yellow]    /[/yellow][grey70](___(___) [/grey70]\n"
-            "                 "
-        ),
-    ),
-    3: WeatherCondition(
-        label="Overcast",
-        icon=(
-            "                 \n"
-            "[grey62]    .--.     [/grey62]\n"
-            "[grey62]  _(    )..   [/grey62]\n"
-            "[grey62] (___.__)__) [/grey62]\n"
-            "                 "
-        ),
-    ),
-    45: WeatherCondition(
-        label="Fog",
-        icon=(
-            "                 \n"
-            "[grey74] _ - _ - _ - [/grey74]\n"
-            "[grey74]  _ - _ - _  [/grey74]\n"
-            "[grey74] _ - _ - _ - [/grey74]\n"
-            "                 "
-        ),
-    ),
-    48: WeatherCondition(
-        label="Rime fog",
-        icon=(
-            "                 \n"
-            "[grey74] _ - _ [white]*[/white] _ - [/grey74]\n"
-            "[grey74]  _ [white]*[/white] _ - _ [white]*[/white] [/grey74]\n"
-            "[grey74] _ - _ [white]*[/white] _ - [/grey74]\n"
-            "                 "
-        ),
-    ),
-    51: WeatherCondition(
-        label="Light drizzle",
-        icon=(
-            "[grey70]    .--.       [/grey70]\n"
-            "[grey70]   (    ).    [/grey70]\n"
-            "[grey70]  (___(__)    [/grey70]\n"
-            "[cornflower_blue]   ʻ  ʻ  ʻ   [/cornflower_blue]\n"
-            "[cornflower_blue] ʻ  ʻ  ʻ    [/cornflower_blue]"
-        ),
-    ),
-    53: WeatherCondition(
-        label="Drizzle",
-        icon=(
-            "[grey70]    .--.       [/grey70]\n"
-            "[grey70]   (    ).    [/grey70]\n"
-            "[grey70]  (___(__)    [/grey70]\n"
-            "[cornflower_blue]   ʻ ʻ ʻ ʻ   [/cornflower_blue]\n"
-            "[cornflower_blue] ʻ ʻ ʻ ʻ    [/cornflower_blue]"
-        ),
-    ),
-    55: WeatherCondition(
-        label="Heavy drizzle",
-        icon=(
-            "[grey62]    .--.       [/grey62]\n"
-            "[grey62]   (    ).    [/grey62]\n"
-            "[grey62]  (___(__)    [/grey62]\n"
-            "[dodger_blue1]  ‚ʻ‚ʻ‚ʻ‚ʻ     [/dodger_blue1]\n"
-            "[dodger_blue1]  ‚ʻ‚ʻ‚ʻ‚ʻ     [/dodger_blue1]"
-        ),
-    ),
-    56: WeatherCondition(
-        label="Freezing drizzle",
-        icon=(
-            "[grey70]    .--.       [/grey70]\n"
-            "[grey70]   (    ).    [/grey70]\n"
-            "[grey70]  (___(__)    [/grey70]\n"
-            "[cornflower_blue]   ʻ [white]*[/white] ʻ [white]*[/white]   \n"
-            "[cornflower_blue]  [white]*[/white] ʻ [white]*[/white] ʻ     [/cornflower_blue]"
-        ),
-    ),
+    -1: WeatherCondition(label="Unknown", icon=_UNKNOWN),
+    0: WeatherCondition(label="Clear sky", icon=_SUN),
+    1: WeatherCondition(label="Mainly clear", icon=_DAY_LIGHT_CLOUD),
+    2: WeatherCondition(label="Partly cloudy", icon=_DAY_LIGHT_CLOUD),
+    3: WeatherCondition(label="Overcast", icon=_OVERCAST),
+    45: WeatherCondition(label="Fog", icon=_FOG),
+    48: WeatherCondition(label="Rime fog", icon=_RIME_FOG),
+    51: WeatherCondition(label="Light drizzle", icon=_LIGHT_CLOUD + _DRIZZLE_LIGHT),
+    53: WeatherCondition(label="Drizzle", icon=_LIGHT_CLOUD + _DRIZZLE),
+    55: WeatherCondition(label="Heavy drizzle", icon=_HEAVY_CLOUD + _DRIZZLE_HEAVY),
+    56: WeatherCondition(label="Freezing drizzle", icon=_LIGHT_CLOUD + _FREEZING_LIGHT),
     57: WeatherCondition(
-        label="Heavy freezing drizzle",
-        icon=(
-            "[grey62]    .--.       [/grey62]\n"
-            "[grey62]   (    ).    [/grey62]\n"
-            "[grey62]  (___(___)    [/grey62]\n"
-            "[cornflower_blue]  ‚ʻ [white]*[/white] ‚ʻ [white]*[/white]  \n"
-            "[cornflower_blue] [white]*[/white] ‚ʻ [white]*[/white] ‚ʻ    [/cornflower_blue]"
-        ),
+        label="Heavy freezing drizzle", icon=_HEAVY_CLOUD + _FREEZING_HEAVY
     ),
-    61: WeatherCondition(
-        label="Light rain",
-        icon=(
-            "[grey70]    .--.       [/grey70]\n"
-            "[grey70]   (    ).    [/grey70]\n"
-            "[grey70]  (___(__)    [/grey70]\n"
-            "[cornflower_blue]   ʻ ʻ ʻ ʻ   [/cornflower_blue]\n"
-            "[cornflower_blue] ʻ ʻ ʻ ʻ    [/cornflower_blue]"
-        ),
-    ),
-    63: WeatherCondition(
-        label="Rain",
-        icon=(
-            "[grey70]    .--.       [/grey70]\n"
-            "[grey70]   (    ).    [/grey70]\n"
-            "[grey70]  (___(__)    [/grey70]\n"
-            "[cornflower_blue]  ‚ʻ‚ʻ‚ʻ‚ʻ     [/cornflower_blue]\n"
-            "[cornflower_blue]  ‚ʻ‚ʻ‚ʻ‚ʻ     [/cornflower_blue]"
-        ),
-    ),
-    65: WeatherCondition(
-        label="Heavy rain",
-        icon=(
-            "[grey62]    .--.       [/grey62]\n"
-            "[grey62]   (    ).    [/grey62]\n"
-            "[grey62]  (___(___)    [/grey62]\n"
-            "[dodger_blue1]  ‚ʻ‚ʻ‚ʻ‚ʻ‚     [/dodger_blue1]\n"
-            "[dodger_blue1]  ‚ʻ‚ʻ‚ʻ‚ʻ‚     [/dodger_blue1]"
-        ),
-    ),
-    66: WeatherCondition(
-        label="Freezing rain",
-        icon=(
-            "[grey70]    .--.       [/grey70]\n"
-            "[grey70]   (    ).    [/grey70]\n"
-            "[grey70]  (___(__)    [/grey70]\n"
-            "[cornflower_blue]   ʻ [white]*[/white] ʻ [white]*[/white]   \n"
-            "[cornflower_blue]  [white]*[/white] ʻ [white]*[/white] ʻ     [/cornflower_blue]"
-        ),
-    ),
+    61: WeatherCondition(label="Light rain", icon=_LIGHT_CLOUD + _DRIZZLE),
+    63: WeatherCondition(label="Rain", icon=_LIGHT_CLOUD + _RAIN),
+    65: WeatherCondition(label="Heavy rain", icon=_HEAVY_CLOUD + _RAIN_HEAVY),
+    66: WeatherCondition(label="Freezing rain", icon=_LIGHT_CLOUD + _FREEZING_LIGHT),
     67: WeatherCondition(
-        label="Heavy freezing rain",
-        icon=(
-            "[grey62]    .--.       [/grey62]\n"
-            "[grey62]   (    ).    [/grey62]\n"
-            "[grey62]  (___(___)    [/grey62]\n"
-            "[dodger_blue1]  ‚ʻ [white]*[/white] ‚ʻ [white]*[/white]  [/dodger_blue1]\n"
-            "[dodger_blue1] [white]*[/white] ‚ʻ [white]*[/white] ‚ʻ    [/dodger_blue1]"
-        ),
+        label="Heavy freezing rain", icon=_HEAVY_CLOUD + _FREEZING_RAIN_HEAVY
     ),
-    71: WeatherCondition(
-        label="Light snow",
-        icon=(
-            "[grey70]    .--.       [/grey70]\n"
-            "[grey70]   (    ).    [/grey70]\n"
-            "[grey70]  (___(__)    [/grey70]\n"
-            "[white]   *    *   [/white]\n"
-            "[white]  *    *    [/white]"
-        ),
-    ),
-    73: WeatherCondition(
-        label="Snow",
-        icon=(
-            "[grey70]    .--.       [/grey70]\n"
-            "[grey70]   (    ).    [/grey70]\n"
-            "[grey70]  (___(__)    [/grey70]\n"
-            "[white]   *  *  *   [/white]\n"
-            "[white]  *  *  *    [/white]"
-        ),
-    ),
-    75: WeatherCondition(
-        label="Heavy snow",
-        icon=(
-            "[grey62]    .--.       [/grey62]\n"
-            "[grey62]   (    ).    [/grey62]\n"
-            "[grey62]  (___(__)    [/grey62]\n"
-            "[white bold]   * * * *    [/white bold]\n"
-            "[white bold]  * * * *     [/white bold]"
-        ),
-    ),
-    77: WeatherCondition(
-        label="Snow grains",
-        icon=(
-            "[grey70]    .--.       [/grey70]\n"
-            "[grey70]   (    ).    [/grey70]\n"
-            "[grey70]  (___(__)    [/grey70]\n"
-            "[white]   •  •  •   [/white]\n"
-            "[white]  •  •  •    [/white]"
-        ),
-    ),
-    80: WeatherCondition(
-        label="Light showers",
-        icon=(
-            '[yellow] __/""[/yellow][grey70].--.      [/grey70]\n'
-            "[yellow]   \\_[/yellow][grey70](    ).   [/grey70]\n"
-            "[yellow]   /[/yellow][grey70](___(__) [/grey70]\n"
-            "[cornflower_blue]     ʻ  ʻ  ʻ [/cornflower_blue]\n"
-            "[cornflower_blue]  ʻ  ʻ  ʻ    [/cornflower_blue]"
-        ),
-    ),
-    81: WeatherCondition(
-        label="Showers",
-        icon=(
-            '[yellow] __/""[/yellow][grey70].--.      [/grey70]\n'
-            "[yellow]   \\_[/yellow][grey70](    ).   [/grey70]\n"
-            "[yellow]   /[/yellow][grey70](___(__) [/grey70]\n"
-            "[cornflower_blue]     ʻ ʻ ʻ ʻ [/cornflower_blue]\n"
-            "[cornflower_blue]  ʻ ʻ ʻ ʻ    [/cornflower_blue]"
-        ),
-    ),
-    82: WeatherCondition(
-        label="Heavy showers",
-        icon=(
-            '[yellow] _/""[/yellow][grey62].-.      [/grey62]\n'
-            "[yellow]  ,\\_[/yellow][grey62](    ).   [/grey62]\n"
-            "[yellow]   /[/yellow][grey62](___(__) [/grey62]\n"
-            "[dodger_blue1]   ‚ʻ‚ʻ‚ʻ‚ʻ   [/dodger_blue1]\n"
-            "[dodger_blue1]   ‚ʻ‚ʻ‚ʻ‚ʻ   [/dodger_blue1]"
-        ),
-    ),
+    71: WeatherCondition(label="Light snow", icon=_LIGHT_CLOUD + _SNOW_LIGHT),
+    73: WeatherCondition(label="Snow", icon=_LIGHT_CLOUD + _SNOW),
+    75: WeatherCondition(label="Heavy snow", icon=_HEAVY_CLOUD + _SNOW_HEAVY),
+    77: WeatherCondition(label="Snow grains", icon=_LIGHT_CLOUD + _SNOW_GRAINS),
+    80: WeatherCondition(label="Light showers", icon=_DAY_LIGHT_CLOUD + _SHOWERS_LIGHT),
+    81: WeatherCondition(label="Showers", icon=_DAY_LIGHT_CLOUD + _SHOWERS),
+    82: WeatherCondition(label="Heavy showers", icon=_DAY_HEAVY_CLOUD + _SHOWERS_HEAVY),
     85: WeatherCondition(
-        label="Light snow showers",
-        icon=(
-            '[yellow] __/""[/yellow][grey70].--.      [/grey70]\n'
-            "[yellow]   \\_[/yellow][grey70](    ).   [/grey70]\n"
-            "[yellow]   /[/yellow][grey70](___(__) [/grey70]\n"
-            "[white bold]     *  *  * [/white bold]\n"
-            "[white bold]  *  *  *   [/white bold]"
-        ),
+        label="Light snow showers", icon=_DAY_LIGHT_CLOUD + _SNOW_SHOWERS_LIGHT
     ),
     86: WeatherCondition(
-        label="Heavy snow showers",
-        icon=(
-            '[yellow] __/""[/yellow][grey62].--.      [/grey62]\n'
-            "[yellow]   \\_[/yellow][grey62](    ).   [/grey62]\n"
-            "[yellow]   /[/yellow][grey62](___(__) [/grey62]\n"
-            "[white bold]    * * * *   [/white bold]\n"
-            "[white bold]   * * * *    [/white bold]"
-        ),
+        label="Heavy snow showers", icon=_DAY_HEAVY_CLOUD + _SNOW_SHOWERS_HEAVY
     ),
-    95: WeatherCondition(
-        label="Thunderstorm",
-        icon=(
-            '[yellow] __/""[/yellow][grey70].--.      [/grey70]\n'
-            "[yellow]   \\_[/yellow][grey70](    ).   [/grey70]\n"
-            "[yellow]   /[/yellow][grey70](___(___) [/grey70]\n"
-            "[cornflower_blue]   ʻ ʻ [yellow blink]⚡[/yellow blink] ʻ ʻ [/cornflower_blue]\n"
-            "[cornflower_blue]  ʻ [yellow blink]⚡[/yellow blink] ʻ ʻ ʻ    [/cornflower_blue]"
-        ),
-    ),
+    95: WeatherCondition(label="Thunderstorm", icon=_DAY_LIGHT_CLOUD + _STORM),
     96: WeatherCondition(
-        label="Thunderstorm with hail",
-        icon=(
-            '[yellow] __/""[/yellow][grey70].--.      [/grey70]\n'
-            "[yellow]   \\_[/yellow][grey70](    ).   [/grey70]\n"
-            "[yellow]   /[/yellow][grey70](___(___) [/grey70]\n"
-            "[cornflower_blue]   ʻ [white]•[/white] [yellow blink]⚡[/yellow blink] ʻ [white]•[/white] [/cornflower_blue]\n"
-            "[cornflower_blue]  [white]•[/white] [yellow blink]⚡[/yellow blink] ʻ [white]•[/white] ʻ    [/cornflower_blue]"
-        ),
+        label="Thunderstorm with hail", icon=_DAY_LIGHT_CLOUD + _STORM_HAIL
     ),
     99: WeatherCondition(
-        label="Severe thunderstorm with hail",
-        icon=(
-            '[yellow] __/""[/yellow][grey62].--.      [/grey62]\n'
-            "[yellow]   \\_[/yellow][grey62](    )..   [/grey62]\n"
-            "[yellow]   /[/yellow][grey62](___(____) [/grey62]\n"
-            "[dodger_blue1]   ʻ [white]•[/white] [yellow blink]⚡[/yellow blink] ʻ [white]•[/white] ʻ [/dodger_blue1]\n"
-            "[dodger_blue1]  [white]•[/white] [yellow blink]⚡[/yellow blink] [white]•[/white] [yellow blink]⚡[/yellow blink] ʻ [white]•[/white] [/dodger_blue1]"
-        ),
+        label="Severe thunderstorm with hail", icon=_DAY_HEAVY_CLOUD + _STORM_SEVERE
+    ),
+}
+
+_NIGHT_OVERRIDES = {
+    0: WeatherCondition(label="Clear night", icon=_MOON),
+    1: WeatherCondition(label="Mainly clear", icon=_NIGHT_LIGHT_CLOUD),
+    2: WeatherCondition(label="Partly cloudy", icon=_NIGHT_LIGHT_CLOUD),
+    80: WeatherCondition(
+        label="Light showers", icon=_NIGHT_LIGHT_CLOUD + _SHOWERS_LIGHT
+    ),
+    81: WeatherCondition(label="Showers", icon=_NIGHT_LIGHT_CLOUD + _SHOWERS),
+    82: WeatherCondition(
+        label="Heavy showers", icon=_NIGHT_HEAVY_CLOUD + _SHOWERS_HEAVY
+    ),
+    85: WeatherCondition(
+        label="Light snow showers", icon=_NIGHT_LIGHT_CLOUD + _SNOW_SHOWERS_LIGHT
+    ),
+    86: WeatherCondition(
+        label="Heavy snow showers", icon=_NIGHT_HEAVY_CLOUD + _SNOW_SHOWERS_HEAVY
+    ),
+    95: WeatherCondition(label="Thunderstorm", icon=_NIGHT_LIGHT_CLOUD + _STORM),
+    96: WeatherCondition(
+        label="Thunderstorm with hail", icon=_NIGHT_LIGHT_CLOUD + _STORM_HAIL
+    ),
+    99: WeatherCondition(
+        label="Severe thunderstorm with hail", icon=_NIGHT_HEAVY_CLOUD + _STORM_SEVERE
     ),
 }
 
 
-def weather_condition(wmo_code: int) -> WeatherCondition:
+def weather_condition(wmo_code: int, is_day: bool = True) -> WeatherCondition:
+    if not is_day and wmo_code in _NIGHT_OVERRIDES:
+        return _NIGHT_OVERRIDES[wmo_code]
+
     return _WEATHER_CONDITIONS.get(wmo_code, _WEATHER_CONDITIONS[-1])
-
-
-if __name__ == "__main__":
-    from rich.console import Console
-    from rich.text import Text
-
-    console = Console()
-
-    for code, condition in _WEATHER_CONDITIONS.items():
-        t = Text()
-        t.append(f"WMO Code: {code} | Label: {condition.label}\n", style="bold yellow")
-        t.append(Text.from_markup(condition.icon))
-        t.append("\n" + "-" * 30 + "\n")
-        console.print(t)
